022693373e3af1ae6c3afff287b62ae2f6d3645a
edison <845292122@qq.com>
2025-02-09 21:56:10 +0800

    feat: 删除旧认证逻辑

--- src/configuration.ts
+++ src/configuration.ts
@@ -9,7 +9,6 @@ import { BizErrorFilter } from './filter/biz.filter'
 import { DefaultErrorFilter } from './filter/default.filter'
 import * as jwt from '@midwayjs/jwt'
 import { ResultMiddleware } from './middleware/result.middleware'
-import { JwtMiddleware } from './middleware/jwt-old.middleware'
 import * as passport from '@midwayjs/passport'
 import * as cron from '@midwayjs/cron'
 import * as cacheManager from '@midwayjs/cache-manager'
@@ -36,7 +35,7 @@ export class MainConfiguration implements ILifeCycle {
 
   async onReady() {
     // add middleware
-    this.app.useMiddleware([ReportMiddleware, ResultMiddleware, JwtMiddleware])
+    this.app.useMiddleware([ReportMiddleware, ResultMiddleware])
     // add filter
     this.app.useFilter([BizErrorFilter, DefaultErrorFilter])
   }
